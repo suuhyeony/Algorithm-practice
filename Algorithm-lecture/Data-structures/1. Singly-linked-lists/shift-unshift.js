64cdf471c6016a661b96af516ace2f079a4f4335
suuhyeony <vv5835@naver.com>
2022-02-20 23:19:57 +0900

    recap Singly linked lists (shift & unshift)

---
+++ Algorithm-lecture/Data-structures/1. Singly-linked-lists/shift-unshift.js
@@ -0,0 +1,38 @@
+class Node { 
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
