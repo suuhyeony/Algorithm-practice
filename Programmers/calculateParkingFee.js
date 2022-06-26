69163954740cc7add427040ea8d47df5ebcded19
suuhyeony <vv5835@naver.com>
2022-06-26 23:18:42 +0900

    add comment to calculateParkingFee

--- Programmers/calculateParkingFee.js
+++ Programmers/calculateParkingFee.js
@@ -1,8 +1,11 @@
+// 2022 KAKAO BLIND 주차 요금 계산 https://programmers.co.kr/learn/courses/30/lessons/92341
+
 function solution(fees, records) {
   const [ baseTime, baseFee, mins, fee ] = fees;
   const answer = [];
   const data = {};
-  
+
+  // 차 번호를 key로 가지고, 입출차 시간을 값으로 가지는 data 생성
   records.forEach((record) => {
     const [ time, carNum ] = record.split(' ');
     if(!data[carNum]) {
@@ -16,8 +19,10 @@ function solution(fees, records) {
     const timeList = data[key];
     let totalTime = 0;
     let idx = 0;
+    // timeList는 [입-출-입-..] 형식
     while(idx < timeList.length) {
       if (idx === timeList.length - 1) {
+        // 홀수이면서 마지막 인덱스일 때
         totalTime += convertToMin('23:59') - convertToMin(timeList[idx]);
         break;
       }
